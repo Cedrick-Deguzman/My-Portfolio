c1db8f1ecc6c9d23f4b11393cf64fb644844bcc2
Cedrick-Deguzman <jonaldcedrick.deguzman@gmail.com>
2023-08-14 11:08:28 +0800

    remove the loading

--- js/script.js
+++ js/script.js
@@ -14,30 +14,30 @@ document.addEventListener("DOMContentLoaded", function() {
         var loader = document.querySelector("#preloader");
         var mainContent = document.querySelector("#content");
 
-        // Show the preloader initially
-        loader.style.display = "block";
-        mainContent.style.display = "none";
+        // // Show the preloader initially
+        // loader.style.display = "block";
+        // mainContent.style.display = "none";
 
-        // Delay the showing of the main content by 2 seconds (2000 milliseconds)
-        var contentDelay = 3000;
+        // // Delay the showing of the main content by 2 seconds (2000 milliseconds)
+        // var contentDelay = 3000;
 
-        // Hide the preloader and show the main content after the delay
-        setTimeout(function() {
-            loader.style.display = "none";
-            mainContent.style.display = "flex";
-        }, contentDelay);
-        const sections = document.querySelectorAll('.section');
+        // // Hide the preloader and show the main content after the delay
+        // setTimeout(function() {
+        //     loader.style.display = "none";
+        //     mainContent.style.display = "flex";
+        // }, contentDelay);
+        // const sections = document.querySelectorAll('.section');
 
-        const sectionObserver = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('show-section'); // Add class to trigger animation
-                    observer.unobserve(entry.target); // Unobserve after animation triggers once
-                }
-            });
-        }, { threshold: 0.5 }); // Adjust threshold based on your preference
+        // const sectionObserver = new IntersectionObserver((entries, observer) => {
+        //     entries.forEach(entry => {
+        //         if (entry.isIntersecting) {
+        //             entry.target.classList.add('show-section'); // Add class to trigger animation
+        //             observer.unobserve(entry.target); // Unobserve after animation triggers once
+        //         }
+        //     });
+        // }, { threshold: 0.5 }); // Adjust threshold based on your preference
 
-        sections.forEach(section => {
-            sectionObserver.observe(section);
-        });
+        // sections.forEach(section => {
+        //     sectionObserver.observe(section);
+        // });
     });
